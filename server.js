e286755ac59c349fa9e77c605fe37c905a3d5436
AsriAzhari123 <211111133@students.mikroskil.ac.id>
2024-11-13 20:06:33 +0700

    up

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ const historyRoutes = require("./src/routes/historyRouter")
 const app = express();
 const port = 3001;
 
-app.use(cors({
-  origin: 'http://localhost:8080', 
-  methods: ['GET', 'POST', 'PATCH', 'DELETE'], 
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 app.use(bodyParser.json());
 
