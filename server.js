bdf0e18b10aaf4b5a90b11a85d60b29f19022cf4
AsriAzhari123 <211111133@students.mikroskil.ac.id>
2024-11-13 19:47:47 +0700

    up

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 3001;
 
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: 'http://localhost:3001', 
   methods: ['GET', 'POST', 'PATCH', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
