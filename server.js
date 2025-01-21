d8d4413b1424df20cfd0c730aea6b757c0c41cf2
AsriAzhari123 <211111133@students.mikroskil.ac.id>
2025-01-22 01:20:49 +0700

    update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
-const uri = 'mongodb+srv://websitetiket:minikube123@cluster0.hgg3m54.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const uri = 'mongodb+srv://websitetiket:minikube123@cluster0.hgg3m54.mongodb.net/eticket';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB with Mongoose!'))
     .catch((error) => console.error('Error connecting to MongoDB:', error));
